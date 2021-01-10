d34d74d596eea4a84f424dccda6e449e841f664a
hatahata7757 <syouki100241@gmail.com>
2021-01-10 23:44:08 +0900

    add: #3 Setting up a Docker container for DB

---
+++ docker/mysql/sql/001-create-tables.sql
@@ -0,0 +1,27 @@
+---- drop ----
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `tasks`:
+
+---- create ----
+create table IF NOT EXISTS `users`
+(
+  `id`               INT(20) AUTO_INCREMENT,
+  `name`             VARCHAR(20) NOT NULL,
+  `created_at`       Datetime DEFAULT NULL,
+  `updated_at`       Datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+create table IF NOT EXISTS `tasks`
+(
+  `id`               INT(20) AUTO_INCREMENT,
+  `name`             VARCHAR(255) NOT NULL,
+  `done`             BOOLEAN DEFAULT false,
+  `description`      VARCHAR(255),
+  `user_id`          INT(20),
+  `created_at`       Datetime DEFAULT NULL,
+  `updated_at`       Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`user_id`),
+    REFERENCES `users`(`users_id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
